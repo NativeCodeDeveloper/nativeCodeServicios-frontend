dc7d73796c54a2e612c78183f89a72fa976a226a
NativeCode <tm.nicolasmachuca@gmail.com>
2026-01-05 08:05:59 -0300

    fix3

--- src/Componentes/CatalogoClient.jsx
+++ src/Componentes/CatalogoClient.jsx
@@ -1,16 +1,13 @@
 "use client"
 
-import {useState, useEffect, Suspense} from 'react';
+import {useState, useEffect} from 'react';
 import {useRouter, useSearchParams} from "next/navigation";
 import { toast } from 'react-hot-toast';
 import {useCarritoGlobal} from "@/ContextosGlobales/CarritoContext";
 import CardNativeCode from "@/Componentes/CardNativeCode";
-import {Michroma} from 'next/font/google';
-import {router} from "next/client";
 import {ShadcnButton} from "@/Componentes/shadcnButton";
 
 export default function CatalogoClient() {
-    const searchParams = useSearchParams();
     const API = process.env.NEXT_PUBLIC_API_URL;
     const [_carrito, setCarrito] = useCarritoGlobal();
     const [listaProductos, setListaProductos] = useState([]);
@@ -62,24 +59,22 @@ export default function CatalogoClient() {
 
 
     return (
-        <Suspense>
-            <div className="min-h-screen bg-black text-white">
-                <div>
-                    <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 mt-8" style={{fontFamily: 'Michroma, sans-serif', letterSpacing: '0.04em', color: '#fff'}}>Selecciona el servicio para continuar con el pago</h1>
-                    <div className="flex flex-wrap gap-10 justify-center">
-                        {listaProductos.map(producto => {
-                            return (
-                                <div key={producto.id_producto}>
-                                    <CardNativeCode titulo={producto.tituloProducto} precio={String(producto.valorProducto)} descripcion={producto.descripcionProducto}/>
-                                    <div className='w-full flex justify-center p-5'>
-                                        <ShadcnButton funcion={()=>pagarProducto(producto)} nombre={'Pagar producto'}/>
-                                    </div>
+        <div className="min-h-screen bg-black text-white">
+            <div>
+                <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 mt-8" style={{fontFamily: 'Michroma, sans-serif', letterSpacing: '0.04em', color: '#fff'}}>Selecciona el servicio para continuar con el pago</h1>
+                <div className="flex flex-wrap gap-10 justify-center">
+                    {listaProductos.map(producto => {
+                        return (
+                            <div key={producto.id_producto}>
+                                <CardNativeCode titulo={producto.tituloProducto} precio={String(producto.valorProducto)} descripcion={producto.descripcionProducto}/>
+                                <div className='w-full flex justify-center p-5'>
+                                    <ShadcnButton funcion={()=>pagarProducto(producto)} nombre={'Pagar producto'}/>
                                 </div>
-                            )
-                        })}
-                    </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
-        </Suspense>
+        </div>
     )
 }
--- src/app/(public)/catalogo/page.jsx
+++ src/app/(public)/catalogo/page.jsx
@@ -1,10 +1,10 @@
-import {suspense} from "react";
+import { Suspense } from "react";
 import CatalogoClient from "@/Componentes/CatalogoClient";
 
 export default function Catalogo() {
     return (
-        <suspense fallback={null}>
-            <CatalogoClient></CatalogoClient>
-        </suspense>
-    )
+        <Suspense fallback={null}>
+            <CatalogoClient />
+        </Suspense>
+    );
 }
